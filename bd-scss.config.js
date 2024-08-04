20d60e76fed6645e018ee7fa69fabb9f762b63f6
João Alcântara <joaosantalcantara@gmail.com>
2024-08-04 15:34:53 -0300

    first commit to setup project theme for discord

--- bd-scss.config.js
+++ bd-scss.config.js
@@ -1,14 +1,14 @@
 /** @type {import('bd-scss/lib/config').Config} */
 export default {
 	meta: {
-		name: 'RadialStatus',
-		author: 'Gibbu#1211',
-		version: '2.0.0',
+		name: 'RoundedStatus',
+		author: 'joaoalcantara#0684',
+		version: '1.0.0',
 		description:
 			'Changes the status icons to wrap around the avatar. Shape can be changed inside the theme file.',
 		source: 'https://github.com/DiscordStyles/RadialStatus',
-		invite: 'ZHthyCw',
-		website: 'https://www.gibbu.me'
+		// invite: 'ZHthyCw',
+		// website: 'https://www.gibbu.me'
 	},
 	base: {
 		output: 'dist/dist'
