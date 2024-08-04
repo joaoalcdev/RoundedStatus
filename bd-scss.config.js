7e746d426fec358ef37704b82b3849acfe8b1419
João Alcântara <joaosantalcantara@gmail.com>
2024-08-04 15:46:39 -0300

    added new links to theme variables

--- bd-scss.config.js
+++ bd-scss.config.js
@@ -5,13 +5,14 @@ export default {
 		author: 'joaoalcantara#0684',
 		version: '1.0.0',
 		description:
-			'Changes the status icons to wrap around the avatar. Shape can be changed inside the theme file.',
-		source: 'https://github.com/DiscordStyles/RadialStatus',
+			'Changes the status icons to wrap around the avatar.',
+		source: 'https://github.com/joaoalcdev/RoundedStatus',
 		// invite: 'ZHthyCw',
 		// website: 'https://www.gibbu.me'
 	},
 	base: {
 		output: 'dist/dist'
 	},
-	baseImport: 'https://discordstyles.github.io/RadialStatus/dist/RadialStatus.css'
+	// baseImport: 'https://discordstyles.github.io/RadialStatus/dist/RadialStatus.css'
+	baseImport: 'https://joaoalcdev.github.io/RoundedStatus/dist/dist/RoundedStatus.css'
 };
